10f0c85250e91464ce3dc8e43e7a3fa670c4d7da
nathongithub <nathan73martinet@gmail.com>
2024-06-18 12:01:05 +0200

    New branch

---
+++ new.py
@@ -0,0 +1,18 @@
+
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Apache2 Debian Default Page: It works</title>
+    <style type="text/css" media="screen">
+  * {
+    margin: 0px 0px 0px 0px;
+    padding: 0px 0px 0px 0px;
+  }
+
+  body, html {
+    padding: 3px 3px 3px 3px;
+
+    background-color: #D8DBE2;
+
+    font-family: Verdana, sans-serif;
+    font-size: 11pt;
+    text-align: center;
+
